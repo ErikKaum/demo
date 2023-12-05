aff8f3f00331a674410d42067bc0e6a478504b55
Erik Kaunismäki <erik.kaum@gmail.com>
2023-12-05 09:49:46 +0200

    more trigger build

--- download.py
+++ download.py
@@ -3,6 +3,7 @@
 # In this example: A Huggingface BERT model
 from transformers import pipeline
 
+
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
     pipeline('fill-mask', model='bert-base-uncased')
